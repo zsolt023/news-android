4852e3b67c4f9878d6c071117855726d796fbbb6
Zsolt Nagy <ext.Zsolt.Nagy@sats.no>
2021-08-06 14:11:33 +0200

    added share and full article buttons and implemented functionality

--- news/app/src/main/java/com/zsolt/news/ui/detail/ArticleActivity.kt
+++ news/app/src/main/java/com/zsolt/news/ui/detail/ArticleActivity.kt
@@ -1,5 +1,7 @@
 package com.zsolt.news.ui.detail
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.squareup.picasso.Picasso
@@ -29,6 +31,19 @@ class ArticleActivity: AppCompatActivity() {
         binding.source.text = article.source.name
         binding.title.text = article.title
         binding.content.text = article.content
+
+        binding.buttonShare.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "text/plain"
+            intent.putExtra(Intent.EXTRA_TEXT, article.url)
+            startActivity(Intent.createChooser(intent, "Share"))
+        }
+
+        binding.buttonFullArticle.setOnClickListener {
+            val i = Intent(Intent.ACTION_VIEW)
+            i.data = Uri.parse(article.url)
+            startActivity(i)
+        }
     }
 
     companion object {
